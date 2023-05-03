2f09b0b979a2e6abf2a6a0d194f8fd45463b592b
TheScriptRailoth <am3718440@gmail.com>
2023-05-03 09:24:21 +0530

    JUET-Admin: Add firebase api control support
    Signed-off-by: TheScriptRailoth <am3718440@gmail.com>

--- app/src/main/java/com/example/juetadmin/add_notice.java
+++ app/src/main/java/com/example/juetadmin/add_notice.java
@@ -1,6 +1,7 @@
 package com.example.juetadmin;
 
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
@@ -8,11 +9,25 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.opengl.ETC1;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class add_notice extends AppCompatActivity {
@@ -21,18 +36,78 @@ public class add_notice extends AppCompatActivity {
     private final int REQ = 1;
     private Bitmap bitmap;
     private ImageView noticeImageView;
+    private EditText noticeTitle;
+    private Button uploadNoticeBtn;
+    private DatabaseReference reference;
+    private StorageReference storageReference;
+    String downloadurl="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_notice);
+
+        reference= FirebaseDatabase.getInstance().getReference();
+        storageReference= FirebaseStorage.getInstance().getReference();
         addImage=findViewById(R.id.addImage);
         noticeImageView=findViewById(R.id.noticeImageView);
+        noticeTitle= findViewById(R.id.noticeTitle);
+        uploadNoticeBtn=findViewById(R.id.uploadNoticeBtn);
         addImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openGallery();
             }
         });
+        uploadNoticeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(noticeTitle.getText().toString().isEmpty()){
+                    noticeTitle.setError("Empty");
+                    noticeTitle.requestFocus();
+
+                } else if (bitmap==null) {
+                    uploadData();
+                }
+                else{
+                    uploadImage();
+                }
+            }
+        });
+    }
+
+    private void uploadImage() {
+        ByteArrayOutputStream baos=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,50,baos);
+        byte[] finalimg= baos.toByteArray();
+        final StorageReference filePath;
+        filePath=storageReference.child("Notice").child(finalimg+"jpg" );
+        final UploadTask uploadTask= filePath.putBytes(finalimg);
+        uploadTask.addOnCompleteListener(this, new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful()){
+                    uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    downloadurl=String.valueOf(uri);
+                                    uploadData();
+                                }
+                            });
+                        }
+                    });
+
+                }
+                else {
+                    Toast.makeText(add_notice.this,"Something went wrong",Toast.LENGTH_SHORT);
+                }
+            }
+        });
+    }
+
+    private void uploadData() {
     }
 
     private void openGallery() {
