d69e70806f30d54c399991c6e47e469445fb6659
TheScriptRailoth <am3718440@gmail.com>
2023-05-11 13:33:50 +0530

    JUET Admin: Updated Notice Adapter
    Signed-off-by: TheScriptRailoth <am3718440@gmail.com>

--- app/src/main/java/com/example/juetadmin/notice/NoticeAdapter.java
+++ app/src/main/java/com/example/juetadmin/notice/NoticeAdapter.java
@@ -1,4 +1,78 @@
 package com.example.juetadmin.notice;
 
-public class NoticeAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.juetadmin.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class NoticeAdapter extends RecyclerView.Adapter <NoticeAdapter.NoticeViewAdapter>{
+
+    private Context context;
+    private ArrayList<NoticeData> list;
+
+    public NoticeAdapter(Context context, ArrayList<NoticeData> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public NoticeViewAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.newsfeed_item_layout,parent,false);
+
+        return new NoticeViewAdapter(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoticeViewAdapter holder, int position) {
+
+        NoticeData currentItem = list.get(position);
+
+        holder.deleteNoticeTitle.setText(currentItem.getTitle());
+
+        try {
+            if (currentItem.getImage() != null) {
+                Picasso.get().load(currentItem.getImage()).into(holder.deleteNoticeImage);
+            }
+        }catch(Exception e){
+                throw new RuntimeException(e);
+            }
+
+        holder.deleteNotice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class NoticeViewAdapter extends RecyclerView.ViewHolder {
+
+        private Button deleteNotice ;
+
+        private TextView deleteNoticeTitle;
+        private ImageView deleteNoticeImage;
+        public NoticeViewAdapter(@NonNull View itemView) {
+            super(itemView);
+            deleteNotice = itemView.findViewById(R.id.deleteNotice);
+            deleteNoticeImage = itemView.findViewById(R.id.deleteNoticeImage);
+            deleteNoticeTitle = itemView.findViewById(R.id.deleteNoticeTitle);
+        }
+    }
 }
